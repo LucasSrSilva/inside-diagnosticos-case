b667505a94f0c11e75393d3fa2615477a742ff69
Lucas Souza Ribeiro da Silva <lucasrsdeven@gmail.com>
2025-08-08 12:43:28 -0300

    feat: error treatment

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   const [users, setUsers] = useState<User[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   const fetchUsers = async () => {
     setIsLoading(true);
@@ -17,6 +18,7 @@ function App() {
       setIsLoading(false);
     } catch (error) {
       console.error("Error fetching users:", error);
+      setHasError(true);
       setIsLoading(false);
     }
   };
@@ -29,11 +31,17 @@ function App() {
     <>
       <h1 className="text-4xl font-title  text-[#e13588] font-bold text-center mt-10">Inside Diagnosticos</h1>
       <h2 className="text-3xl font-normal text-center my-5">Case técnico</h2>
-      <section className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mx-10" aria-label="usuarios registrados">
-        {isLoading
-          ? Array.from({ length: 12 }).map((_, idx) => <Skeleton key={idx} />)
-          : users.map((user: User) => <UserCard key={user.id} user={user} />)
-        }
+      <section className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto p-10" aria-label="usuarios registrados">
+        {isLoading && (
+          Array.from({ length: 12 }).map((_, idx) => <Skeleton key={idx} />)
+        )}
+        {!isLoading && hasError && (
+          <div className="text-center border border-red-300 p-8 rounded-lg bg-red-100">
+            <p className="text-black text-3xl font-bold mb-10">Erro ao buscar usuários</p>
+            <button onClick={fetchUsers} className="bg-blue-700 text-white p-2 rounded-lg hover:bg-blue-800">Tentar novamente</button>
+          </div>
+        )}
+        {!isLoading && !hasError && users.map((user: User) => <UserCard key={user.id} user={user} />)}
       </section>
     </>
   )
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,10 @@
 const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 export const api = {
     get: async (path: string) => {
-        console.log("BASE_URL:", BASE_URL)
         const response = await fetch(`${BASE_URL}/${path}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         return response.json();
     },
 }
